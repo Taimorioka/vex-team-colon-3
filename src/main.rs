065d4dee9237a5e81ac2cbbb90cc5010a43e4baa
taiMorioka <thetaimorioka@gmail.com>
2024-09-27 15:26:07 -0700

    feat: add deadzone

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,17 @@ impl Compete for Robot {
         loop {
             let forward = self.controller.left_stick.y().unwrap_or_default() as f64;
             let turn = self.controller.right_stick.x().unwrap_or_default() as f64;
-            let left_voltage = (forward + turn) * Motor::MAX_VOLTAGE;
-            let right_voltage = (forward - turn) * Motor::MAX_VOLTAGE;
-
-            // Set the drive motors to our arcade control values.
-            self.motor_left.set_voltage(left_voltage).ok();
-            self.motor_right.set_voltage(right_voltage).ok();
+            if forward.abs() > 0.05 || turn.abs() > 0.05 {
+                let left_voltage = (turn + forward) * Motor::MAX_VOLTAGE;
+                let right_voltage = (turn - forward) * Motor::MAX_VOLTAGE;
+    
+                // Set the drive motors to our arcade control values.
+                self.motor_left.set_voltage(left_voltage).ok();
+                self.motor_right.set_voltage(right_voltage).ok();
+            } else {
+                self.motor_left.brake(BrakeMode::Brake).ok();
+                self.motor_right.brake(BrakeMode::Brake).ok();
+            }
 
 
             sleep(Duration::from_millis(10)).await;
